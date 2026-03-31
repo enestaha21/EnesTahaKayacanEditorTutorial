e12713838d34e9e502ef824e1ebab51db7a8214e
enestaha21 <240404010@ogr.kent.edu.tr>
2026-03-31 21:41:07 +0300

    Programming essentials completed.

---
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Collectible : MonoBehaviour
+{
+    public float rotationSpeed;
+    public GameObject onCollectEffect;
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(0, rotationSpeed, 0);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")){
+            Destroy(gameObject);
+            Instantiate(onCollectEffect, transform.position, transform.rotation);
+        }
+        
+    }
+
+    
+
+}
